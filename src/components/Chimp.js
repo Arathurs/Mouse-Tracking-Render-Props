1732c534737d6ada69c88bef29ed6d37d129b04b
Royalthius <arathurs@gmail.com>
2020-08-20 01:35:35 -0400

    Changed file path.

--- src/components/Chimp.js
+++ src/components/Chimp.js
@@ -4,7 +4,7 @@ export class Chimp extends React.Component {
     render() {
       const mouse = this.props.mouse;
       return (
-        <img src="../../../public/img/chimp.png" style={{ position: 'absolute', left: mouse.x, top: mouse.y }} alt="He's going BANANASSSS!!!"/>
+        <img src="https://arathurs.github.io/Mouse-Tracking-Render-Props/public/img/chimp.png" style={{ position: 'absolute', left: mouse.x, top: mouse.y }} alt="He's going BANANASSSS!!!"/>
       );
     }
 }
