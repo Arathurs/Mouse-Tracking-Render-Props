f79415dd222dc6484c7aa6c3a493db49160c2232
Royalthius <arathurs@gmail.com>
2020-08-20 00:06:53 -0400

    Changed filepath.

--- src/components/Chimp.js
+++ src/components/Chimp.js
@@ -4,7 +4,7 @@ export class Chimp extends React.Component {
     render() {
       const mouse = this.props.mouse;
       return (
-        <img src="../../public/img/chimp.png" style={{ position: 'absolute', left: mouse.x, top: mouse.y }} alt="He's going BANANASSSS!!!"/>
+        <img src="../../../public/img/chimp.png" style={{ position: 'absolute', left: mouse.x, top: mouse.y }} alt="He's going BANANASSSS!!!"/>
       );
     }
 }
